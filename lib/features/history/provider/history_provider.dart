1445b4db92617295633a91267ae2b148a9f47228
Prabhat Jana <itsmeprabhatjana@gmail.com>
2026-02-06 20:46:33 +0530

    feat(history): set up history Riverpod providers
    - Create historyRepositoryProvider
    - Set up historyViewModelProvider
    - Configure provider dependencies
    - Initialize on app start

---
+++ lib/features/history/provider/history_provider.dart
@@ -0,0 +1,12 @@
+import 'package:fileflow/features/history/model/history_item.dart';
+import 'package:fileflow/features/history/repository/history_repository.dart';
+import 'package:fileflow/features/history/viewmodel/history_viewmodel.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
+
+final historyRepositoryProvider = Provider((ref) => HistoryRepository());
+
+final historyViewModelProvider =
+    StateNotifierProvider<HistoryViewModel, List<HistoryItem>>((ref) {
+      return HistoryViewModel(ref.read(historyRepositoryProvider));
+    });
