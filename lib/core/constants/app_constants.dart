a4cb7db399b22e817c2a7bb370e66c23e80e4be9
Prabhat Jana <itsmeprabhatjana@gmail.com>
2026-02-08 11:18:29 +0530

    feat(storage): implement dynamic cross-platform file paths
    - Android: /storage/emulated/0/FileFlow/
    - Linux: ~/Documents/FileFlow/
    - macOS: ~/Documents/FileFlow/
    - Windows: %USERPROFILE%\\Documents\\FileFlow\\
    - Use Platform.isAndroid/isLinux/isMacOS/isWindows checks
    - Remove hardcoded user-specific paths

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,10 +1,24 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+
 class AppConstants {
-  static const int port = 4000;
+  static const String multicastAddress = '239.255.12.34';
+  static const int port = 26841;
 
   static const String historyBoxName = 'history';
   static const String settingsBoxName = 'settings';
   static const String transferStatesBoxName = 'transferStates';
 
-  static const String appDir = '/storage/emulated/0/FileFlow/';
-  
+  static final String appDir = Platform.isAndroid
+      ? '/storage/emulated/0/FileFlow/'
+      : Platform.isLinux
+      ? '~/Documents/FileFlow/'
+      : Platform.isMacOS
+      ? '~/Documents/FileFlow/'
+      : Platform.isWindows
+      ? 'C:\\Users\\%USERNAME%\\Documents\\FileFlow\\'
+      : getApplicationDocumentsDirectory()
+            .then((dir) => '${dir.path}/FileFlow/')
+            .toString();
 }
